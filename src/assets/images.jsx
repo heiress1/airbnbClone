563e9362054e9d65b978d7f49df67776b4f239fa
heiress1 <aires.zheng1@gmail.com>
2024-08-22 16:23:29 -0400

    used props for card info and images

---
+++ src/assets/images.jsx
@@ -0,0 +1,4 @@
+export { default as katie } from './katie.png'
+export { default as star } from './star.png'
+
+
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,41 @@
 import React from "react"
-import katie from "../assets/katie-zaferes.png"
+// import img from "../assets/katie-zaferes.png"
 import star from "../assets/star.png"
+import * as images from "../assets/images.jsx"
 
-export default function Card(){
+export default function Card(props){
     return (
+        
         <div className="cards">
-            <div className="katie">
+            
                 
-                <img src={katie} alt="katie zeferes" />
+            <img className="card" src={images[props.portrait]} alt={props.altText} />
+                <div className="rating">
+                    <img className="star" src={images[props.star]}/>
+                    <span>  {props.rating} </span>
+                    <span className="grey">({props.reviewCount}) • {props.country}</span>
+                   
                 
-                <div className="star">
-                    <img src={star} alt="star icon" />
-                    <span>5.0 <span className="grey">(6) • USA</span></span>
+            
+            
+                
+                    {/* <div className="rating">
+                        <img src={star} alt="star icon" />
+                        <span>5.0 <span className="grey">(6) • USA</span></span>
+                    </div>
+                    
+                    <h2>Life lessons with Katie Zaferes</h2>
+                    <p><span className="bold">From $136</span> / person</p> */ }
                 </div>
+
+                <p>{props.title}</p>
+                <p><b> From ${props.price}</b> / person
+                </p>
+                    
+                    
                 
-                <h2>Life lessons with Katie Zaferes</h2>
-                <p><span className="bold">From $136</span> / person</p>
-            </div>
 
-
-        </div>
+         </div>
+        
     )
 }
