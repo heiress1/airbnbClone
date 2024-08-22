1e5af88188bcbd21e28ebdfa29fab97b5ff54d38
heiress1 <aires.zheng1@gmail.com>
2024-08-22 16:40:49 -0400

    used props to update cards and images

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,35 @@ import Navbar from "./components/Navbar"
 import './App.css'
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import cardData from "./data"
 
 
 export default function App() {
-  
+  const cardElements = cardData.map(card => {
+    return <Card id={card.id} title={card.title} description={card.description} price={card.price} coverImg={card.coverImg} rating={card.stats[rating]} reviewCount={card.stats[reviewCount]} location={card.location} openSpots={card.openSpots}/> 
+  })
 
   return (
     <div>
-      
       <Navbar/>
       <Hero/>
-      <Card/>
+      {cardElements}
+      {/* <Card
+        portrait="katie"
+        star="star"
+        altText='Katie Zaferes'
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      /> */}
+     
+    
+      
     </div>
   )
+  
 }
 
 
