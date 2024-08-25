2d36fa0a27e510ef1de3772e8478bf6aa9555509
heiress1 <aires.zheng1@gmail.com>
2024-08-24 23:33:08 -0400

    used a object property to pass in props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,32 @@ import React from "react";
 // import img from "../assets/katie-zaferes.png"
 import star from "../assets/star.png";
 import * as images from "../assets/images.jsx";
+import { getImageURL } from "/src/image-util";
 
 export default function Card(props) {
+
+  let status;
+    if (props.cards.openSpots === 0) {
+        status = "SOLD OUT";
+    } else if (props.cards.location === "Online") {
+        status = "ONLINE";
+    }
+
   return (
     <div className="cards">
-        <div className="cardContainer">
+        {(props.cards.openSpots === 0 || props.cards.location === "Online" ) && <div className="status">{status}</div>}
+        {/* <div className="cardContainer"> */}
           <img className="card" src={props.portrait} alt={props.altText} />
-        </div>
+        {/* </div> */}
 
         <div className="rating">
           <img className="star" src={star} alt="star rating image" />
-          <span> {props.rating} </span>
+          <span> {props.cards.stats.rating} </span>
           <span className="grey">
-            ({props.reviewCount}) • {props.country}
+            ({props.cards.stats.reviewCount}) • {props.cards.location}
           </span>
         
-      </div>
+        </div>
 
       <p>{props.title}</p>
       <p>
